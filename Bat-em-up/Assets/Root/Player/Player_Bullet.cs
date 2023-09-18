ef3d853f1df48278fe0b1534c2d9c3aeea323fdd
MrRobododo <mathieurebolledo@yahoo.fr>
2023-09-18 22:21:43 +0200

    Test

--- Bat-em-up/Assets/Root/Player/Player_Bullet.cs
+++ Bat-em-up/Assets/Root/Player/Player_Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,17 +13,24 @@ public class Player_Bullet : MonoBehaviour
     public float smoothTime = 0.5f;
     private Transform player;
 
+    public float power = 15f;
     [Header("Speed")]
-    public float accPower = 15f;
-    public float minSize = 0.5f;
-    public float maxSize = 7f;
-
-    [Header ("Scale")]
+    
     public float minSpeed = 5f;
     public float maxSpeed = 30F;
+
+    [Header ("Scale")]
+    
+    public float minSize = 0.5f;
+    public float maxSize = 7f;
     public float scaleUpFactor = 1.4f;
     private Vector3 currentMaxScale;
-    public float maxScale = 4;
+
+    [Header ("Time")]
+    public float maxSlowDuration = 15f;
+    private float lerpTime;
+    private float currentLerpTime;
+
     public float slowDownFactor = 0.1f;
     public float scaleDownFactor = 0.1f;
 
@@ -75,14 +83,18 @@ public class Player_Bullet : MonoBehaviour
 
             if (rigidBody.velocity.magnitude > minSpeed)
             {
-                rigidBody.velocity -= rigidBody.velocity.normalized * slowDownFactor * Time.deltaTime;
-                targetVelocity = rigidBody.velocity;
+                currentLerpTime + = Time.deltaTime;
+                float t = currentLerpTime / lerpTime;
+                transfrom.localScale = Vector3.one * Mathf(minSize, maxSize, t);
+                //rigidBody.velocity -= rigidBody.velocity.normalized * slowDownFactor * Time.deltaTime;
+                //targetVelocity = rigidBody.velocity;
             }
             else
             {
                 targetVelocity = (player.position - (Vector3)rigidBody.position).normalized * minSpeed;
             }
 
+            /*  
             // Update target scale to match slow down
             float normalizedVelocityMagnitude = Mathf.Clamp01(rigidBody.velocity.magnitude / originalVelocityMagnitude);
 
@@ -94,7 +106,7 @@ public class Player_Bullet : MonoBehaviour
             //float smoothedScale = Mathf.Lerp(bulletGameObject.transform.localScale.x, targetScale, Time.deltaTime / smoothTime);
             bulletGameObject.transform.localScale = Vector3.one * targetScale;
             
-
+            */
             // Apply smoothed velocity
             rigidBody.velocity = Vector2.SmoothDamp(
                 rigidBody.velocity,
@@ -110,9 +122,19 @@ public class Player_Bullet : MonoBehaviour
        // Debug.Log("Call Hit");
         startingSlowDown = false;
         velocityTimeElapsed = 0;
-        GetComponent<Rigidbody2D>().velocity = direction * (accPower + GetComponent<Rigidbody2D>().velocity.magnitude);
-        currentMaxScale = transform.localScale * scaleUpFactor;
-        transform.localScale = currentMaxScale;
+
+        CalculateLerpTime();
+        //lerpTime = maxSlowDuration * ()
+        
+        //GetComponent<Rigidbody2D>().velocity = direction * (accPower + GetComponent<Rigidbody2D>().velocity.magnitude);
+        //currentMaxScale = transform.localScale * scaleUpFactor;
+        //transform.localScale = currentMaxScale;
+    }
+
+    private void CalculateLerpTime()
+    {
+        float clampedScale = Mathf.Clamp(transform.localScale.x * scaleUpFactor, minSize, maxSize);
+        lerpTime = maxSlowDuration * (clampedA/ maxSize)
     }
 }
 
